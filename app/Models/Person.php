5ea4252c2b84aa14b807c0128bd7c3ef582883b8
kartik <78898735+Misfit64@users.noreply.github.com>
2025-11-26 14:14:52 +0530

    update: model methods

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     protected $fillable = [
+        'family_tree_id',
         'first_name',
         'last_name',
         'gender',
@@ -20,4 +21,58 @@ class Person extends Model
     protected $casts = [
         'gender' => Gender::class,
     ];
+
+
+    public function parents()
+    {
+        return $this->belongsToMany(Person::class, 'relationships', 'person_id', 'relative_id')
+            ->where('relationship_type', RelationshipType::Parent->value);
+    }
+
+    public function children()
+    {
+        return $this->belongsToMany(Person::class, 'relationships', 'person_id', 'relative_id')
+            ->where('relationship_type', RelationshipType::Child->value);
+    }
+
+    public function spouses()
+    {
+        return $this->belongsToMany(Person::class, 'relationships', 'person_id', 'relative_id')
+            ->where('relationship_type', RelationshipType::Spouse->value);
+    }
+
+    public function siblings()
+    {
+        $parents = $this->parents;
+
+        $siblings = collect();
+
+        foreach ($parents as $parent) {
+            $siblings = $siblings->merge($parent->children);
+        }
+
+        return $siblings->unique('id')->where('id', '!=', $this->id);
+    }
+
+    public function canonicalPerson()
+    {
+        return $this->belongsTo(Person::class, 'canonical_person_id');
+    }
+
+    public function familyTree()
+    {
+        return $this->belongsTo(FamilyTree::class);
+    }
+
+    public function relationships()
+    {
+        return $this->hasMany(Relationship::class);
+    }
+
+    public function fullName():Attribute
+    {
+        return Attribute::make(
+            get: fn () => trim($this->first_name . ' ' . ($this->last_name ?? '')),
+        );
+    }
 }
--- app/Models/Relationship.php
+++ app/Models/Relationship.php
@@ -19,6 +19,37 @@ class Relationship extends Model
         'relationship_type' => RelationshipType::class,
         'relationship_subtype' => RelationshipSubType::class,
     ];
-
     
+    public function familyTree()
+    {
+        return $this->belongsTo(FamilyTree::class);
+    }
+
+    public function person()
+    {
+        return $this->belongsTo(Person::class,'person_id');
+    }
+
+    public function relative()
+    {
+        return $this->belongsTo(Person::class, 'relative_id');
+    }
+
+    #[Scope]
+    public function parents(Builder $query)
+    {
+        return $query->where('relationship_type', RelationshipType::Parent->value);
+    }
+
+    #[Scope]
+    public function children(Builder $query)
+    {
+        return $query->where('relationship_type', RelationshipType::Child->value);
+    }
+
+    #[Scope]
+    public function spouses(Builder $query)
+    {
+        return $query->where('relationship_type', RelationshipType::Spouse->value);
+    }
 }
